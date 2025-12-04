0c2529b59ffa598cf4e1d391db9ee37c799642fe
Akshay <mandaakshay20@gmail.com>
2025-12-04 12:34:24 +0530

    wrote description

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ const states = [{
         description: "Indore is a city in west-central India. It\'s known for the 7-story Rajwada Palace and the Lal Baag Palace, which date back to Indore\'s 19th-century Holkar dynasty.",
         landmarks: [{
             name: "Mhow",
-            description: "Dr. Ambedkar Nagar, commonly known as Mhow..."
+            description: "Dr. Ambedkar Nagar, commonly known as Mhow, is a cantonment in the Indore district in Madhya Pradesh state of India. It is located 23 kilometres south-west of Indore city, towards Mumbai on the old Mumbai-Agra Road."
         }, {
             name: "Dewas",
             description: "Dewas is a city in the Malwa region..."
