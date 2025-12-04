a4e8e1709c4f710556c740bbc1382ee103ca04ad
Akshay <mandaakshay20@gmail.com>
2025-12-04 16:46:38 +0530

    wrote description

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,7 @@ const states = [{
         description: "DBhopal is a city in the central Indian state of Madhya Pradesh. It\'s one of India\'s greenest city. There are two main lakes, the Upper Lake and the Lower Lake.",
         landmarks: [{
             name: "MANIT",
-            description: "Maulana Azad National Institute of Technology Bhopal..."
+            description: "Maulana Azad National Institute of Technology Bhopal is a public technical university located in Bhopal, Madhya Pradesh, India. It is part of a group of publicly funded institutions in India known as National Institutes of Technology."
         }, {
             name: "Berasia",
             description: "Berasia is a landmark and a nagar palika..."
