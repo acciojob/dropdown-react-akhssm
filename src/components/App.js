dde6960274eb0347ba1f23fecfed3f62656c4203
Akshay <mandaakshay20@gmail.com>
2025-12-04 12:29:10 +0530

    wrote description

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ const states = [{
     description: "Madhya Pradesh, a large state in central India, retains landmarks from eras throughout Indian history.",
     city: [{
         name: "Indore",
-        description: "Indore is a city in west-central India...",
+        description: "Indore is a city in west-central India. It\'s known for the 7-story Rajwada Palace and the Lal Baag Palace, which date back to Indore\'s 19th-century Holkar dynasty.",
         landmarks: [{
             name: "Mhow",
             description: "Dr. Ambedkar Nagar, commonly known as Mhow..."
